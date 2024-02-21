4824bb69bbf0890ac197e386ade03b4aaa0ab831
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2024-02-21 12:02:55 -0300

    details in feature

--- src/main/java/com/example/SpotifyData/client/ArtistResponse.java
+++ src/main/java/com/example/SpotifyData/client/ArtistResponse.java
@@ -3,14 +3,18 @@ package com.example.SpotifyData.client;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import java.util.List;
+
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ArtistResponse {
     private String name;
+    private List<String> genres;
     private ArtistFollowers followers;
     private Integer popularity;
 
-    public ArtistResponse(String name, ArtistFollowers followers, Integer popularity) {
+    public ArtistResponse(String name, List<String> genres, ArtistFollowers followers, Integer popularity) {
         this.name = name;
+        this.genres = genres;
         this.followers = followers;
         this.popularity = popularity;
     }
@@ -24,6 +28,14 @@ public class ArtistResponse {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
     }
 
     public ArtistFollowers getFollowers() {
